963ce7d1b622a9ac2ac771a7a956bd63d39dae13
Mikk Pristavka <mikk@ikald.us>
2025-01-13 13:41:32 +0200

    Run `go fmt` to fix formatting issues

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 )
 
 func main() {
-    filePath := parseFlags()
-    validateFilePath(filePath)
-    r := openZipFile(filePath)
-    defer r.Close()
+	filePath := parseFlags()
+	validateFilePath(filePath)
+	r := openZipFile(filePath)
+	defer r.Close()
 
-    workDir := getWorkingDirectory()
-    fileName := extractFileName(filePath)
+	workDir := getWorkingDirectory()
+	fileName := extractFileName(filePath)
 
-    baseDir := filepath.Join(workDir, fileName, "src")
-    processZipFiles(r, baseDir, workDir, fileName)
+	baseDir := filepath.Join(workDir, fileName, "src")
+	processZipFiles(r, baseDir, workDir, fileName)
 }
